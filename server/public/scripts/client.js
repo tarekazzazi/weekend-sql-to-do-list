4fd9e8ad5716d8b312b2c0185458141c93e78cab
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-05-28 20:40:24 -0500

    added click listener and inputs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,12 +4,28 @@ $(document).ready(OnReady);
 function OnReady() {
     console.log("JQ ready!");
 
+    clickListener();
+
     getTasks();
 }
 
-function getTasks(){
+ function clickListener(){
+          console.log('In click listener');
+        $('#addTaskBtn').on('click', function (){
+            console.log("in addTaskBtn");
+            // get user inputs put in a object
+            let taskToAdd = {
+                name: $('#taskName').val(),
+                isComplete: $('#isTaskComplete').val()
+            };
+            // call that object
+            // console.log tests
+            console.log('New task to be added is',taskToAdd);
+            saveNewTask(taskToAdd);
+        })
+ }
 
-   
+function getTasks(){
 
     $.ajax({
         type: "GET",
@@ -23,3 +39,7 @@ function getTasks(){
         console.log('ahh error!',err);
     });
 }
+
+// function saveNewTask(newTask) {
+    
+// }
